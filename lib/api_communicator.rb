a2ee31fcc7c5a1de39fed7d95c021af8f3dd03ff
Han Xu <hanxu27@gmail.com>
2019-04-23 16:45:16 -0400

    Apis and iteration finals

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,27 +2,51 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+def pull_data(link)
+# helper method to get data and convert into hash
+  response_string = RestClient.get(link)
   response_hash = JSON.parse(response_string)
+end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def get_character_movies_from_api(character_name)
+# call pull_data to get hash then iterate each entry to check if names match
+# if names match then return the links of films
+# else return "Not found"
+  result = pull_data('http://www.swapi.co/api/people/')["results"]
+
+  result.each do |data|
+    if character_name == data["name"].downcase
+      return data["films"]
+    end
+  end
+  "Not found"
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  # if films is "Not found" return error
+  # else film is an array of links, compare links to 
+  # episode_id to get movie titles
+  # return array of movie titles
+  movie_data = pull_data("https://www.swapi.co/api/films/")["results"]
+  results = []
+  if films == "Not found"
+    puts "Character not found"
+    return "Character not found"
+  end
+  films.each do |link|
+    movie_data.each do |data|
+      if data["episode_id"] == link[-2].to_i
+        puts data["title"]
+        results << data["title"]
+      end
+    end
+  end
+  results
 end
 
 def show_character_movies(character)
+# calling get_character_movies_from_api to get array of
+# links/films. Calling print_movies to print titles to output
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
+require 'pry'
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome Star Wars Lovers"
 end
 
 def get_character_from_user
+  # gets user's input, then downcase input
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  name = gets.chomp.downcase
 end
